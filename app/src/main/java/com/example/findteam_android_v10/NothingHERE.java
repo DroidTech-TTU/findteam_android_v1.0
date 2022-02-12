e1266d3d8d29ebfb61f9834627c2efe2ba304b3b
anlqt89 <82059787+anlqt89@users.noreply.github.com>
2022-02-12 17:57:18 -0600

    Main activities

---
+++ app/src/main/java/com/example/findteam_android_v10/NothingHERE.java
@@ -0,0 +1,30 @@
+package com.example.findteam_android_v10;
+
+public class NothingHERE {
+    public void nothing(){
+//        NavController navController = Navigation.findNavController(this, R.id.activity_main_nav_host_fragment);
+//        BottomNavigationView bottomNavigationView = findViewById(R.id.activity_main_bottom_navigation_view);
+//        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+//
+//         <fragment
+//        android:id="@+id/activity_main_nav_host_fragment"
+//        android:name="androidx.navigation.fragment.NavHostFragment"
+//        android:layout_width="match_parent"
+//        android:layout_height="wrap_content"
+//        app:defaultNavHost="true"
+//        app:layout_constraintLeft_toLeftOf="parent"
+//        app:layout_constraintRight_toRightOf="parent"
+//        app:layout_constraintTop_toTopOf="parent"
+//        app:navGraph="@navigation/nav_graph" />
+//
+//    <com.google.android.material.bottomnavigation.BottomNavigationView
+//        android:id="@+id/activity_main_bottom_navigation_view"
+//        android:layout_width="match_parent"
+//        android:background="@color/white"
+//        android:layout_height="wrap_content"
+//        app:layout_constraintBottom_toBottomOf="parent"
+//        app:layout_constraintLeft_toLeftOf="parent"
+//        app:layout_constraintRight_toRightOf="parent"
+//        app:menu="@menu/navigation_menu"/>
+    }
+}
