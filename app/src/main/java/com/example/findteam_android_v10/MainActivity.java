566cdb5802b944f4232ff467b9b8b41096174597
anlqt89 <82059787+anlqt89@users.noreply.github.com>
2022-02-12 13:20:30 -0600

    summary
    New

---
+++ app/src/main/java/com/example/findteam_android_v10/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.findteam_android_v10;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
