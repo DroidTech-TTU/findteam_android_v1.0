ac784d4bb46600dac2df96d021b9e14af1702361
John Manacup <john.manacup@gmail.com>
2022-02-12 16:40:34 -0600

    Add Login Activity

--- app/src/main/java/com/example/findteam_android_v10/RegisterActivity.java
+++ app/src/main/java/com/example/findteam_android_v10/RegisterActivity.java
@@ -3,12 +3,11 @@ package com.example.findteam_android_v10;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
+        setContentView(R.layout.activity_register);
     }
 }
