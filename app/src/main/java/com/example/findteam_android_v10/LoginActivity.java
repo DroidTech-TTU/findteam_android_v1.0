252424acdf3b576620651641ddbefd3497109dce
John Manacup <john.manacup@gmail.com>
2022-02-18 16:14:10 -0600

    Login and Register Change

--- app/src/main/java/com/example/findteam_android_v10/LoginActivity.java
+++ app/src/main/java/com/example/findteam_android_v10/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,11 +12,33 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Button registerInLoginBtn, loginInLoginBtn;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-    }
+        registerInLoginBtn = findViewById(R.id.register_btn);
+        loginInLoginBtn = findViewById(R.id.login_btn);
 
+        //register account
+        registerInLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //login account
+        loginInLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
     public void loginSuccessful(View view){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
--- app/src/main/java/com/example/findteam_android_v10/RegisterActivity.java
+++ app/src/main/java/com/example/findteam_android_v10/RegisterActivity.java
@@ -1,10 +1,16 @@
 package com.example.findteam_android_v10;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -14,10 +20,49 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    ImageView profilePic;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        ImageView addPicBtn;
+        Button registerBtn;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        profilePic = findViewById(R.id.profilePic);
+        addPicBtn = findViewById(R.id.profPicBtn);
+        registerBtn = findViewById(R.id.registerAct_btn);
+
+        //get the profile image from the phone storage
+        addPicBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mGetContent.launch("image/*");
+            }
+        });
+
+        //does the process after the user register
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+    ActivityResultLauncher<String> mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(),
+            new ActivityResultCallback<Uri>() {
+                @Override
+                public void onActivityResult(Uri uri) {
+                    // Handle the returned Uri
+                    profilePic.setImageURI(uri);
+                }
+
+            });
+
+
 }
