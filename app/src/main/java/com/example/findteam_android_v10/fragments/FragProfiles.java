fd7f95b395cf158a62e6dbb85c0511b05063fa60
John Manacup <john.manacup@gmail.com>
2022-02-23 15:04:43 -0600

    updates

--- app/src/main/java/com/example/findteam_android_v10/fragments/FragProfiles.java
+++ app/src/main/java/com/example/findteam_android_v10/fragments/FragProfiles.java
@@ -11,6 +11,12 @@ import androidx.fragment.app.Fragment;
 
 import com.example.findteam_android_v10.R;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import co.lujun.androidtagview.TagContainerLayout;
+
 public class FragProfiles extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,6 +27,11 @@ public class FragProfiles extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.frag_profile, container, false);
+
+        List<String> skills = Arrays.asList("C#", "Python", "Java", "Software Developer","C#", "Python", "Java", "Software Developer","C1#", "P1ython", "Ja1va", "Softw1are Developer","Software Dev2eloper","C1#2", "P12ython", "Ja1v2a", "Softw1are De2veloper");
+        TagContainerLayout mTagContainerLayout = view.findViewById(R.id.skills_tag);
+        mTagContainerLayout.setTags(skills);
+
         return view;
     }
 }
