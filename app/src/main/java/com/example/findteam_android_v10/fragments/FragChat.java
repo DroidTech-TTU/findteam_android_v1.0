408cf91a3961e9ba7bd9a71afaa476507c506dd3
Rory Eckel <16675082+roryeckel@users.noreply.github.com>
2022-03-31 18:41:41 -0500

    Chat progress

--- app/src/main/java/com/example/findteam_android_v10/fragments/FragChat.java
+++ app/src/main/java/com/example/findteam_android_v10/fragments/FragChat.java
@@ -8,14 +8,21 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.findteam_android_v10.R;
 
+import org.json.JSONObject;
+
 public class FragChat extends Fragment {
+
+    private RecyclerView chatListRecyclerView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.frag_chat, container, false);
+        chatListRecyclerView = view.findViewById(R.id.chatListRecyclerView);
         return view;
     }
 
