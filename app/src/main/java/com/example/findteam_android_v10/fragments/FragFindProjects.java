99a553f91cf5fdba646c53ffb82446a947dbe216
anlqt89 <82059787+anlqt89@users.noreply.github.com>
2022-04-04 00:48:51 -0500

    asd

--- app/src/main/java/com/example/findteam_android_v10/fragments/FragFindProjects.java
+++ app/src/main/java/com/example/findteam_android_v10/fragments/FragFindProjects.java
@@ -1,66 +1,185 @@
 package com.example.findteam_android_v10.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.example.findteam_android_v10.CreateProjectActivity;
+import com.example.findteam_android_v10.FindTeamClient;
+import com.example.findteam_android_v10.LoginActivity;
 import com.example.findteam_android_v10.R;
+import com.example.findteam_android_v10.adapters.MyProjectsAdapter;
 import com.example.findteam_android_v10.adapters.SearchTabAdapter;
+import com.example.findteam_android_v10.classes.Project;
 import com.google.android.material.tabs.TabLayout;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 public class FragFindProjects extends Fragment {
 
-    public static final String TAG = "FragFindProjects";
-    TabLayout tabLayout;
-    ViewPager2 viewPager;
-    SearchTabAdapter searchTabAdapter;
+    public static String TAG = "FragFindProjects";
+    public static final String GET_MY_SEARCH = "project/search";
+    public static final int CREATE_PROJECT_CODE = 1133;
+    RecyclerView rvContacts;
+    ImageButton btSearchMyProjects;
+    EditText etSearchMyProjects;
+    JSONArray jsonProjects;
+    MyProjectsAdapter adapter;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "On create");
+
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.frag_find_projects, container, false);
+        this.rvContacts = (RecyclerView) view.findViewById(R.id.rvMyProjectsSearch);
+        this.btSearchMyProjects = view.findViewById(R.id.btSearchMyProjects);
+        this.etSearchMyProjects = view.findViewById(R.id.etSearchMyProjects);
+
+        try {
+            getAllProjects();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        this.btSearchMyProjects.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    search();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
         return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-//        tabLayout= view.findViewById(R.id.tabLayout);
-//        viewPager= view.findViewById(R.id.viewPager);
-//
-//        tabLayout.addTab(tabLayout.newTab().setText("Home"));
-//        tabLayout.addTab(tabLayout.newTab().setText("Sport"));
-//        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-//        searchTabAdapter = new SearchTabAdapter(view.getContext(), getFragmentManager(), 2, getLifecycle());
-//        viewPager.setAdapter(searchTabAdapter);
-//
-//
-//        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-//            @Override
-//            public void onTabSelected(TabLayout.Tab tab) {
-//                viewPager.setCurrentItem(tab.getPosition());
-//            }
-//
-//            @Override
-//            public void onTabUnselected(TabLayout.Tab tab) {
-//
-//            }
-//
-//            @Override
-//            public void onTabReselected(TabLayout.Tab tab) {
-//
-//            }
-//        });
     }
+
+    public void getAllProjects() throws JSONException {
+        String URL = GET_MY_SEARCH;
+        Log.d(TAG,"getAllProjects:" + URL );
+        FindTeamClient.get(URL, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                Log.i(TAG, "the status code for this request is: " + statusCode );
+                try {
+                    jsonProjects = new JSONArray(new String(responseBody));
+                    Log.i(TAG, "Data: " + jsonProjects);
+                    adapter = new MyProjectsAdapter(getContext(), jsonProjects);
+                    rvContacts.setAdapter(adapter);
+                    rvContacts.setLayoutManager(new LinearLayoutManager(getActivity()));
+                    Toast.makeText(getContext(), "Successfully Get Projects:", Toast.LENGTH_SHORT).show();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Log.e(TAG, "the status code for this request is" + statusCode);
+                Toast.makeText(getContext(), "Get Projects Failure: ", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void search() throws JSONException {
+
+        String URL = GET_MY_SEARCH;
+        Log.d(TAG,"searchEmpty:" + URL );
+        FindTeamClient.get(URL, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                Log.i(TAG, "the status code for this request is: " + statusCode );
+                try {
+                    jsonProjects = new JSONArray(new String(responseBody));
+                    Log.i(TAG, "Data: " + jsonProjects);
+                    String searchKey = etSearchMyProjects.getText().toString();
+                    if(!searchKey.trim().isEmpty()){
+                        jsonProjects = Project.search(jsonProjects, searchKey);
+
+
+                    }
+                    Log.i(TAG, "Search Results: " + jsonProjects);
+                    adapter.clear();
+                    adapter.addAll(jsonProjects);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Log.e(TAG, "the status code for this request is" + statusCode);
+                Toast.makeText(getContext(), "Search Projects Failure: ", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+//    @Override
+//    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+//        super.onViewCreated(view, savedInstanceState);
+////        tabLayout= view.findViewById(R.id.tabLayout);
+////        viewPager= view.findViewById(R.id.viewPager);
+////
+////        tabLayout.addTab(tabLayout.newTab().setText("Home"));
+////        tabLayout.addTab(tabLayout.newTab().setText("Sport"));
+////        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+////        searchTabAdapter = new SearchTabAdapter(view.getContext(), getFragmentManager(), 2, getLifecycle());
+////        viewPager.setAdapter(searchTabAdapter);
+////
+////
+////        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+////            @Override
+////            public void onTabSelected(TabLayout.Tab tab) {
+////                viewPager.setCurrentItem(tab.getPosition());
+////            }
+////
+////            @Override
+////            public void onTabUnselected(TabLayout.Tab tab) {
+////
+////            }
+////
+////            @Override
+////            public void onTabReselected(TabLayout.Tab tab) {
+////
+////            }
+////        });
+//    }
 }
