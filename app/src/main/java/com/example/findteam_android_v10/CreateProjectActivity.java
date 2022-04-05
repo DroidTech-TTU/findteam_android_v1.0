de5dc4a0f312cccfa06d5f03cd803a956c208304
John Manacup <john.manacup@gmail.com>
2022-04-04 22:18:27 -0500

    Fixed bottom layout

--- app/src/main/java/com/example/findteam_android_v10/CreateProjectActivity.java
+++ app/src/main/java/com/example/findteam_android_v10/CreateProjectActivity.java
@@ -315,12 +315,8 @@ public class CreateProjectActivity extends AppCompatActivity {
         Intent intent = new Intent(Intent.ACTION_PICK,
                 MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
 
-        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
-        // So as long as the result is not null, it's safe to use the intent.
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            // Bring up gallery to select a photo
-            startActivityForResult(intent, PICK_PHOTO_CODE);
-        }
+        // Bring up gallery to select a photo
+        startActivityForResult(intent, PICK_PHOTO_CODE);
     }
 
     public Bitmap loadFromUri(Uri photoUri) {
